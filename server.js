59fd498f162bacab71329d35fa116fe3f644866e
greghorne <greghorne@hotmail.com>
2019-01-16 19:48:27 +0000

    initial commit and layout additions

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app     = express();
+
+app.set('view engine', 'ejs');
+
+// require ("./js/constants.js")
+
+app.get('/', function(req, res) {
+    res.render('pages/index');
+});
+
+var server = app.listen(3000, '0.0.0.0', function () {
+    console.log("app listening on http://%s:%s", server.address().address, server.address().port);
+});
