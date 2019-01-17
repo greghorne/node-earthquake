a4d59bfa373dfb093ccf9f5c33afe9fe8e049045
greghorne <greghorne@hotmail.com>
2019-01-17 16:55:36 +0000

    updated code; unable to resolve CORS issue with SockJS

--- server.js
+++ server.js
@@ -5,9 +5,10 @@ app.set('view engine', 'ejs');
 
 // require ("./js/constants.js")
 
-app.get('/', function(req, res) {
+app.get('/', function(req, res, next) {
     res.render('pages/index');
 });
+
 
 var server = app.listen(3000, '0.0.0.0', function () {
     console.log("app listening on http://%s:%s", server.address().address, server.address().port);
