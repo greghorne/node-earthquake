47e91a2e36a67305d77cb2cb36f0b1faff30df99
greghorne <greghorne@hotmail.com>
2019-01-24 21:38:07 +0000

    renamed constants and removed function variable that is no longer used

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const CONST_PORT = process.env.CONST_PORT;
-const CONST_HOST = process.env.CONST_HOST;
+const CONST_APP_PORT = process.env.CONST_APP_PORT;
+const CONST_APP_HOST = process.env.CONST_APP_HOST;
 
 var express = require('express');
 var app     = express();
@@ -8,12 +8,12 @@ app.set('view engine', 'ejs');
 
 // require ("./js/constants.js")
 
-app.get('/', function(req, res, next) {
+app.get('/', function(req, res) {
     res.render('pages/index');
 });
 
 
 // var server = app.listen(3000, '0.0.0.0', function () {
-var server = app.listen(CONST_PORT, CONST_HOST, function () {
+var server = app.listen(CONST_APP_PORT, CONST_APP_HOST, function () {
     console.log("app listening on http://%s:%s", server.address().address, server.address().port);
 });
